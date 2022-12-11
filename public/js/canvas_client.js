a73e97bb4bf39b44f6d6d48c493a9b2b067723b5
Hengjun <hengjunluo@gmail.com>
2022-12-10 21:44:34 -0700

    merged the independent servers
    now we can go through  sign up - login - create room - draw. still need to implement join room

--- public/js/canvas_client.js
+++ public/js/canvas_client.js
@@ -1,16 +1,17 @@
-/*
+
 //---get room id from url----
 let urlString = window.location.search;
 let urlParams = new URLSearchParams(urlString);
 let roomID = urlParams.get("roomId");
 let username = urlParams.get("userName");
 
+/*
 //--send room id to next page
 window.location.href = "NEXT_PAGE.html"+"?roomId="+roomId+"$userName="+username;
 */
 
 
-const socket = io('http://localhost:3000');
+const socket = io('http://localhost:4000');
 socket.on('draw', drawLine)
 let HEIGHT, WIDTH, STROKE;
 HEIGHT = 1000;
@@ -29,7 +30,6 @@ let colorButtons = document.getElementsByClassName('color_button');
 Array.from(colorButtons).forEach((colorButton) => {
     colorButton.addEventListener('click', () => {
         currentColor = getComputedStyle(colorButton, null).getPropertyValue("background-color");
-        console.log(currentColor)
         canvasContext.strokeStyle = currentColor;
         Array.from(colorButtons).forEach((button) => {
             button.style.border = 'grey none';
@@ -59,17 +59,14 @@ canvas.addEventListener(myDown, e => {
     xOffset = e.clientX - canvas.getBoundingClientRect().left;
     yOffset = e.clientY - canvas.getBoundingClientRect().top;
     drawing = true;
-    console.log('down')
 });
 window.addEventListener(myUp, e => {
     drawing = false;
 });
 canvas.addEventListener(myMove, e => {
     if (drawing) {
-        console.log('move')
         let xInCanvas = e.clientX - canvas.getBoundingClientRect().left;
         let yInCanvas = e.clientY - canvas.getBoundingClientRect().top;
-        console.log(currentColor)
         update(xOffset, yOffset, xInCanvas, yInCanvas, currentColor);
         toDB();
         xOffset = xInCanvas;
@@ -77,22 +74,22 @@ canvas.addEventListener(myMove, e => {
     }
 })
 
+socket.emit('join', JSON.stringify({room: roomID}));
+
 function update(xStart, yStart, xEnd, yEnd, color) {
-    socket.emit('update', JSON.stringify({ xStart, yStart, xEnd, yEnd, color }));
+    socket.emit('update', JSON.stringify({room: roomID, data: { xStart, yStart, xEnd, yEnd, color }}));
 }
 function toDB() {
     if (!updating) {
-        socket.emit('toDB', JSON.stringify(canvas.toDataURL()))
+        socket.emit('toDB', JSON.stringify({room: roomID, data: canvas.toDataURL()}))
         updating = true;
         setTimeout(() => {
             updating = false;
-            socket.emit('toDB', JSON.stringify(canvas.toDataURL()))
+            socket.emit('toDB', JSON.stringify({room: roomID, data: canvas.toDataURL()}))
         }, 1000)
     }
 }
 function drawLine(data) {//draw a line in canvas
-    console.log('draw')
-    data = JSON.parse(data);
     canvasContext.beginPath();
     canvasContext.moveTo(data.xStart, data.yStart);
     canvasContext.lineTo(data.xEnd, data.yEnd);
@@ -106,7 +103,7 @@ function drawLine(data) {//draw a line in canvas
 
 socket.on('history', (data) => {
     if (data) {
-        data = JSON.parse(data)
+        console.log('hist')
         let img = new Image;
         img.onload = function () {
             canvasContext.drawImage(img, 0, 0); // Or at whatever offset you like
--- userDB_API.js
+++ userDB_API.js
@@ -1,9 +1,10 @@
-var sqlite3 = require("sqlite3");
-var express= require("express");
-let path=require("path");
-// exports.static = require('serve-static');
-
-var app=express();
+let sqlite3 = require("sqlite3");
+let express = require('express'),
+    app = express(),
+    http = require('http'),
+    socketIO = require('socket.io'),
+    server, io,
+    path = require("path");
 app.use(express.json());
 
 //provid the main page to client by get api
@@ -12,6 +13,10 @@ app.get('/', function (req, res) {
 });
 //public the codes
 app.use(express.static(__dirname + '/public'));
+server = http.Server(app);
+server.listen(4000);//server listening to local host port 4000
+io = socketIO(server);//build a scoket io server based on express http server
+
 
 // app.get("/create_data",(req,res)=>{
 //     var db = new sqlite3.Database('./public/userAccount.db',(err,data)=>{
@@ -29,53 +34,53 @@ app.use(express.static(__dirname + '/public'));
 // })
 
 
-app.post('/signup',(req,res)=>{
+app.post('/signup', (req, res) => {
     var user = req.body;
-    let ans={stat:"",content:""}
-    if(user!=undefined){
+    let ans = { stat: "", content: "" }
+    if (user != undefined) {
         console.log(user)
-        var db = new sqlite3.Database('./public/db/database.db',(err,data)=>{
-            if(!err){
-                let exist=false;
-                db.all('select * from userInfo where username = "'+user.username+'"', (err,data)=>{
-                    if(!err){
-                        console.log("length: "+data.length);
-                        if(data.length>0){
-                            exist=true;
+        var db = new sqlite3.Database('./public/db/database.db', (err, data) => {
+            if (!err) {
+                let exist = false;
+                db.all('select * from userInfo where username = "' + user.username + '"', (err, data) => {
+                    if (!err) {
+                        console.log("length: " + data.length);
+                        if (data.length > 0) {
+                            exist = true;
                             console.log("stop!")
                         }
                         console.log(exist)
-                    if(exist){
-                        ans['stat']=0;
-                        ans['content']="Username already";
-                        console.log("Stop here!!!")
-                        res.send(JSON.stringify(ans))
-                    }else{
-                        db.run('INSERT INTO userInfo(username,password) values("'+user.username+'","'+user.password+'")',(err)=>{
-                            if(!err){
-                                ans['stat']=1;
-                                ans['content']='Sign up successfully!';
-                                return res.send(JSON.stringify(ans))
+                        if (exist) {
+                            ans['stat'] = 0;
+                            ans['content'] = "Username already";
+                            console.log("Stop here!!!")
+                            res.send(JSON.stringify(ans))
+                        } else {
+                            db.run('INSERT INTO userInfo(username,password) values("' + user.username + '","' + user.password + '")', (err) => {
+                                if (!err) {
+                                    ans['stat'] = 1;
+                                    ans['content'] = 'Sign up successfully!';
+                                    return res.send(JSON.stringify(ans))
                                 }
-                            else{
-                                ans['stat']=0;
-                                ans['content']='Error!';
-                                console.log(err);
-                                return res.send(JSON.stringify(ans))
-                            }
-                        })
-                    }
-                    }else{
+                                else {
+                                    ans['stat'] = 0;
+                                    ans['content'] = 'Error!';
+                                    console.log(err);
+                                    return res.send(JSON.stringify(ans))
+                                }
+                            })
+                        }
+                    } else {
                         res.send("Your id is incorrect so,please check your Id....    ")
                         console.log("check you id");
                     }
                 })
 
-   
-                }
-            })
+
+            }
+        })
     }
-    else{
+    else {
         console.log('getting undefined data');
         return res.send('undefined data')
     }
@@ -84,28 +89,28 @@ app.post('/signup',(req,res)=>{
 
 
 
-app.post("/signin",(req,res)=>{
-    var password=req.body.password
-    var username=req.body.username
-    let ans={stat:"",content:""}
+app.post("/signin", (req, res) => {
+    var password = req.body.password
+    var username = req.body.username
+    let ans = { stat: "", content: "" }
 
-    console.log("email: "+username+"\npassword: "+password)
-    var db= new sqlite3.Database("./public/db/database.db",(err,data)=>{
-       if(!err){
-           db.all('SELECT username,password FROM userInfo where username="'+username+'" and password="'+password+'"',(err,data)=>{
+    console.log("email: " + username + "\npassword: " + password)
+    var db = new sqlite3.Database("./public/db/database.db", (err, data) => {
+        if (!err) {
+            db.all('SELECT username,password FROM userInfo where username="' + username + '" and password="' + password + '"', (err, data) => {
                 console.log(data)
-               if(data.length==1){
-                    ans['stat']=1;
-                    ans['content']='You are logged in successfully!';
-                   return res.send(JSON.stringify(ans))
-               }
-               else{
-                    ans['stat']=0;
-                    ans['content']='You have entered wrong username or password!';
-                   return res.send(JSON.stringify(ans))
-               }
-           })
-       }
+                if (data.length == 1) {
+                    ans['stat'] = 1;
+                    ans['content'] = 'You are logged in successfully!';
+                    return res.send(JSON.stringify(ans))
+                }
+                else {
+                    ans['stat'] = 0;
+                    ans['content'] = 'You have entered wrong username or password!';
+                    return res.send(JSON.stringify(ans))
+                }
+            })
+        }
     })
 })
 
@@ -142,43 +147,92 @@ app.post("/signin",(req,res)=>{
 // })
 
 // createRoomTable
-app.post("/createRoom",(req,res)=>{
+app.post("/createRoom", (req, res) => {
     var room = req.body;
-    var ans={stat:"",content:""}
-    var db = new sqlite3.Database('./public/db/database.db',(err,data)=>{
-    if(!err){
-                // db.run('CREATE TABLE IF NOT EXISTS rooms(roomID text primary key, openStatus text, publicStatus text , owner text, canvasHistory text)',(err)=>{
-                //     if(!err){
-                //         console.log('rooms table is created sucessfully!')
-                //     }
-                //     else{
-                //         console.log(err.message)
-                //     }
-                //     res.send(JSON.stringify("ok"))
-                // // }
-                //db.run('INSERT INTO rooms values("'+room.roomID+'","'+user.password+'")',(err)=>{
-                db.run('INSERT INTO rooms values("'+room.roomID+'","'+room.openStatus+'","'+room.publicStatus+'","'+room.owner+'","'+room.canvasHistory+'")',(err)=>{
-                    if(!err){
-                        console.log("inserted!")
-                        ans['stat']=1;
-                        ans['content']='create successfully!';
-                        res.send(JSON.stringify(ans))
-                        }
-                    else{
-                        ans['stat']=0;
-                        ans['content']='Error!';
-                        console.log(err);
-                        res.send(JSON.stringify(ans))
-                    }
-                })
-            }
+    var ans = { stat: "", content: "" }
+    var db = new sqlite3.Database('./public/db/database.db', (err, data) => {
+        if (!err) {
+            // db.run('CREATE TABLE IF NOT EXISTS rooms(roomID text primary key, openStatus text, publicStatus text , owner text, canvasHistory text)',(err)=>{
+            //     if(!err){
+            //         console.log('rooms table is created sucessfully!')
+            //     }
+            //     else{
+            //         console.log(err.message)
+            //     }
+            //     res.send(JSON.stringify("ok"))
+            // // }
+            //db.run('INSERT INTO rooms values("'+room.roomID+'","'+user.password+'")',(err)=>{
+            db.run('INSERT INTO rooms values("' + room.roomID + '","' + room.openStatus + '","' + room.publicStatus + '","' + room.owner + '","' + room.canvasHistory + '")', (err) => {
+                if (!err) {
+                    console.log("inserted!")
+                    ans['stat'] = 1;
+                    ans['content'] = 'create successfully!';
+                    res.send(JSON.stringify(ans))
+                }
+                else {
+                    ans['stat'] = 0;
+                    ans['content'] = 'Error!';
+                    console.log(err);
+                    res.send(JSON.stringify(ans))
+                }
+            })
+        }
     });
 })
-    
 
+//--------------------------------------------------------------------testing without actual db---------------------------------
+let historyForDB = {};
 
-app.listen(4000,()=>{
-    console.log("your server has been started..   ");
+io.on('connection', async (socket) => {
+    socket.on('join', async (data) => {
+        console.log('join');
+        let roomID = JSON.parse(data).room;
+        //join the default testing room
+        socket.join(roomID);
+        //sync the canvas history to new user
+        if (historyForDB[roomID]) {// if there exists history for the current room
+            socket.emit('history', historyForDB[roomID]);
+        }
+
+        /*let currentCanvasHostory = getCanvasHistory(roomID);
+        if(currentCanvasHostory){
+            socket.emit('history', currentCanvasHostory);
+        }*/
+    })
+
+    socket.on('update', (newUpdate) => {//need to modify to multi-room version
+        newUpdate = JSON.parse(newUpdate);
+        let roomID = newUpdate.room;
+        let data = newUpdate.data;
+        io.sockets.in(roomID).emit('draw', data);
+    })
+    socket.on('toDB', (newCanvas) => {// need to accept room id in the future-----------------------------------------------
+        newCanvas = JSON.parse(newCanvas);
+        let roomID = newCanvas.room;
+        let data =  newCanvas.data;
+        historyForDB[roomID] = data;
+        //updateCanvasHistory(data,roomID);
+    })
 })
 
+function updateCanvasHistory(canvas, room) {
+    db.run('UPDATE room SET canvas_history = ? WHERE roomId =?', [canvas, room], function (err) {
+        if (err) {
+            console.log(err.message);
+        } else {
+            console.log('Room: ' + room + ', canvas history updated.');
+        }
+    })
+}
 
+function getCanvasHistory(room) {
+    let canvasHistory = null;
+    db.run('SELECT canvas_history FROM room WHERE roomId = ?', [room], function (err, data) {
+        if (err) {
+            console.log(err.message);
+        } else {
+            canvasHistory = data;
+        }
+    })
+    return canvasHistory;
+}
