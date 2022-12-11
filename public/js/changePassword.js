a99bab797bba60bc13f438886aa4a10c067f5ae6
parrrry <parrychirakorn@gmail.com>
2022-12-10 21:56:31 -0700

    adding saves

---
+++ public/js/changePassword.js
@@ -0,0 +1,143 @@
+// Reference https://www.geeksforgeeks.org/how-to-reset-change-password-in-node-js-with-passport-js/
+
+
+
+// const express = require('express')
+// const bodyParser = require('body-parser')
+// const mongoose = require("mongoose");
+// const passport = require("passport");
+// const passportLocalMongoose 
+//     = require("passport-local-mongoose");
+  
+// mongoose.connect(
+// "mongodb://localhost:27017/passport-forget", {
+//     useNewUrlParser: true
+// });
+
+async function sendRequest(){
+
+
+    let usernameIn = document.getElementById("username").value;
+    let passwordIn = document.getElementById("password").value;
+    let newpasswordIn = document.getElementById("newPassword").value;
+    console.log("sendRequest from changePassword.js")
+    console.log("User name: "+usernameIn+"\nPassword: "+passwordIn+"\nNEWPassword: "+newpasswordIn);
+
+    const response = await fetch("/changepassword/", {
+        method: 'POST',
+        headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+                username: usernameIn,
+                password: passwordIn,
+                newpassword: newpasswordIn
+                }),
+        });
+        // response.json().then(data => {
+        response.json().then(data => {
+            console.log(typeof data);
+            if(data['stat']==1){
+                alert("Changed Password Successfully!!!");
+            }else{
+                alert("Changed Password fail!");
+            }
+            console.log(data);
+        });
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const app = express()
+  
+// app.use(passport.initialize());
+  
+// const userSchema = new mongoose.Schema({
+//     username: String,
+//     password: String,
+// });
+  
+// userSchema.plugin(passportLocalMongoose);
+  
+// const User = new mongoose.model("User", userSchema);
+  
+// passport.use(User.createStrategy());
+  
+// passport.serializeUser(function (user, done) {
+//     done(null, user.id);
+// });
+  
+// passport.deserializeUser(function (id, done) {
+//     User.findById(id, function (err, user) {
+//         done(err, user);
+//     });
+// });
+  
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }))
+  
+// // app.get('/register', function (req, res) {
+// //     res.sendFile('register.html', {
+// //         root: __dirname
+// //     });
+// // });
+  
+// app.get('/changepassword', function (req, res) {
+//     res.sendFile('changepassword.html', {
+//         root: __dirname
+//     });
+// });
+  
+// app.post('/register', function (req, res) {
+//     User.register({
+//         username: req.body.username
+//     }, req.body.password, function (err) {
+//         if (err) {
+//             res.send(err);
+//         } else {
+//             res.send('successfully registered')
+//         }
+//     });
+// });
+
+
+// app.post('/changepassword', function (req, res) {
+//     User.findByUsername(req.body.username, (err, user) => {
+//         if (err) {
+//             res.send(err);
+//         } else {
+//             user.changePassword(req.body.oldpassword, 
+//             req.body.newpassword, function (err) {
+//                 if (err) {
+//                     res.send(err);
+//                 } else {
+//                     res.send('successfully change password')
+//                 }
+//             });
+//         }
+//     });
+// });
+  
+  
+app.listen(3000);
