8265ca40ee41572f7da967ca4ac76704c987d24b
XiangyuLiualex <xiangyuliualex@gmail.com>
2022-12-12 13:17:50 -0700

    add back button and page jumping from sign up to home page

--- public/js/signup.js
+++ public/js/signup.js
@@ -21,6 +21,7 @@ async function sendRequest(){
         console.log(typeof data);
         if(data['stat']==1){
             alert(data['content']);
+            window.location.href = 'homepage.html'+'?userName='+usernameIn;
         }else{
             alert(data['content']);
         }
